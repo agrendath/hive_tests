bf60162322790fadadd60ab748947eb69368ba16
agrendath <mendo.leal@gmail.com>
2024-03-31 13:47:16 +0200

    Start of decryption loop

--- decrypt.py
+++ decrypt.py
@@ -1,3 +1,12 @@
+import os
 from calculate_block_size import calculate_block_size
 from calculate_start_offsets import calculate_start_offsets
 
+infected_files_dir = "infected_files"
+original_files_dir = "original_files"
+
+for file in os.listdir(path=original_files_dir):
+    if not os.path.isfile(file):
+        continue
+
+    # further code here
