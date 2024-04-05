cdb0db3d8fc7b0472f407bbf30fb81683e401c4f
agrendath <mendo.leal@gmail.com>
2024-04-05 15:23:42 +0200

    More work good travail

--- calculate_start_offsets.py
+++ calculate_start_offsets.py
@@ -1,13 +1,16 @@
-import sys
-import base64 
-from base64 import urlsafe_b64encode
+import base64
+
+def extract_base64_part(file_name):
+    parts = file_name.split(".")
+    return parts[-2]
 
 def calculate_start_offsets(file_name):
-    r = base64.urlsafe_b64decode(file_name)[16:]
+    encoded = extract_base64_part(file_name)
+    r = base64.urlsafe_b64decode(encoded + '==')[16:]
     r1 = int.from_bytes(r[:8], "little")
     r2 = int.from_bytes(r[8:], "little")
     sp1 = r1%0x900000
     sp2 = r2%0x9FFC00
 
-    return (hex(sp1), hex(sp2))
+    return (sp1, sp2)
 
--- decrypt.py
+++ decrypt.py
@@ -26,26 +26,36 @@ for original_file in original_files :
     # Get the corresponding infected (encrypted) file
     infected_file = getInfectedFileFromOriginal(infected_files, original_file)
     print("File: " + original_file + " -> " + infected_file + "...")
+
+    if_content = open(infected_files_dir + "/" + infected_file, "rb").read()
+    of_content = open(original_files_dir + "/" + original_file, "rb").read()
+    print("Length of file: " + str(len(if_content)))
     
     # Main algorithm
     nbs = calculate_block_size(infected_files_dir + "/" + infected_file) ## USING ALGO 1
     SP1, SP2 = calculate_start_offsets(infected_file)  # stores sp1 and sp2 in hex ##USING ALGO 2
 
-    size = os.path.getsize(infected_file)
+    size = os.path.getsize(infected_files_dir + "/" + infected_file)
     iter = size//(0x1000 + nbs)
     offset = 0
-    EQS = {}
-    for i in range (iter+1):
+    EQS = set()
+    for i in range (iter + 1):
+        print("Iteration " + str(i) + "/" + str(iter) + "...")
         if i == iter :
             ## From paper:
             ## If the last block size is more than 0x1000 bytes, 0x1000 bytes of the last block
             ## starting from the end of the file are encrypted. Otherwise the entire block is encrypted
             if size - 0x1000 > offset:
                 offset += size - offset - 0x1000
-        for j in range(0xFFF +1):
+        for j in range(0xFFF + 1):
             O1 = offset%0x100000
             O2 = offset%0x1000
-            EQS.add((SP1 + O1, SP2 + O2, infected_file[offset] ^ original_file[offset]))
+            #print("Offset 1: " + str(O1))
+            #print("Offset 2: " + str(O2))
+            if offset > len(if_content) - 1 or offset > len(of_content) - 1:
+                print("[!] END OF FILE REACHED")
+                break
+            EQS.add((SP1 + O1, SP2 + O2, if_content[offset] ^ of_content[offset]))
             offset += 1
         offset += nbs
 
@@ -56,19 +66,22 @@ if EQS == {}:
 EK = [None] * 0xA00000
 E = copy.deepcopy(list(EQS)[random.randint(0, len(EQS) - 1)])
 EK[E[0]] = random.randint(0, 255)
-EQS = tuple(EQS)
+EQS = list(EQS)
 
 sentinelle = len(EQS)
 
+result = None
 while len(EQS) == sentinelle:
     for EQ in EQS:
         if ((EK[EQ[0]]) == None)  and ((EK[EQ[1]]) == None):
             continue
         elif ((EK[EQ[0]]) != None)  and ((EK[EQ[1]]) == None):
-            EK[EQ[1]] = EK[EQ[0]] ^ EK[EQ[2]]      
+            EK[EQ[1]] = EK[EQ[0]] ^ int(EK[EQ[2]] or 0)     
         elif ((EK[EQ[0]]) == None)  and ((EK[EQ[1]]) != None):
-            EK[EQ[0]] = EK[EQ[1]] ^ EK[EQ[2]]      
+            EK[EQ[0]] = EK[EQ[1]] ^ int(EK[EQ[2]] or 0)      
         elif ((EK[EQ[0]]) != None) and ((EK[EQ[1]]) != None):
-            EQS.pop(EQ)
+            result = EQ
+            EQS.pop(EQS.index(EQ))
 
-print(EQS)
+print("EQS:", EQS)
+print("Popped EQ:", result)
